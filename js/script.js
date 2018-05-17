f3cb568f3a65dd6383c396abd2b3ba805e7ea9a5
Kamalesh Palanisamy <kamalesh800@gmail.com>
2018-05-17 17:53:32 +0400

    Completed my website

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-var h1 = document.querySelector("h1");
+var h1 = document.querySelector("#welcome");
 var text = "Welcome To My Website";
 
 var i =0;
 setInterval(function(){
 	h1.textContent = text.slice(0,i+1);
-	i++
-},400)
+	i++;
+	
+},200)
+
+
+
+	
